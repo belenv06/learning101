655bf30c039e468bfce143668a15169be5e67b81
Jean Pier <diegojeanpier.dc.@gmail.com>
2022-03-04 17:51:38 -0500

    ruta home arreglada

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  root to: 'pages#home'
   resources :prospects, only: [ :index, :show, :new, :create, :update, :edit ] do
     resources :comments, only: [ :create]
   end
