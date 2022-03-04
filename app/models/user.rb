5f77288903ef6c3125a0ae1a20dfe4ea6e36ef43
rosalinda belen <rosabelen.v0610@gmail.com>
2022-03-03 23:22:06 -0500

    estilos de tablas y se agrego el campo user al modelo prospecto

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :comments, dependent: :destroy
+  has_many :prospects
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   # :registerable,
